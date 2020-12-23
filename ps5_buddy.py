ff55657d6d9f97941b3672ed2b18825a9158977c
Marvin Lüthe <marvin-luethe@gmx.de>
2020-12-23 12:42:29 +0100

    added confirmation message

--- ps5_buddy.py
+++ ps5_buddy.py
@@ -3,16 +3,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 from twilio.rest import Client
+from datetime import datetime
 
 # define params
-os.environ['TWILIO_ACCOUNT_SID'] = '<your accound sid>'
+os.environ['TWILIO_ACCOUNT_SID'] = '<your sid>'
 os.environ['TWILIO_AUTH_TOKEN'] = '<your token>'
-from_whatsapp_number='whatsapp:<+your sandbox number>'
-to_whatsapp_number='whatsapp:<+your number>'
+from_whatsapp_number='whatsapp:<twilio nr>'
+to_whatsapp_number='whatsapp:+<your nr>'
+
+
 
 client = Client()
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+client.messages.create(body='Läuft!!!',
+  from_=from_whatsapp_number,
+  to=to_whatsapp_number)
 
 def clear_cache(driver):
     """Clear the cookies and cache for the ChromeDriver instance."""
@@ -31,8 +37,18 @@ def clear_cache(driver):
 def main():
     """Go on amazon and check if PS5 is available, then clear cache and do it again."""
     i=0
+    time_store = set()
     # keep looping between amazon and browser clear_cache
     while True:
+        now = datetime.now()
+        now_tuple = (now.hour, now.minute)
+        # send confirmation
+        if (now_tuple not in time_store) and (now_tuple[1] == 0):
+            time_store.add(now_tuple)
+            client.messages.create(body='Läuft!!!',
+              from_=from_whatsapp_number,
+              to=to_whatsapp_number)
+
         # go on amazon
         driver.get('https://www.amazon.de/Sony-Interactive-Entertainment-PlayStation-5/dp/B08H98GVK8/ref=sr_ \
             1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=ps5&qid=1606155052&sr=8-1&th=1')
@@ -63,11 +79,11 @@ def main():
             os.system('play -nq -t alsa synth {} sine {}'.format(5, 440))
             print('PS5 kaufen!!!')
             break
-        i+=1
         print(str(i),availability)
         clear_cache(driver)
         time.sleep(15)
-
+        i+=1
 
 if __name__ == '__main__':
     main()
+
